ed96d4390f7fd8409b9a47376198fed133e218ef
brol <brol31@gmail.com>
2018-02-19 15:20:11 +0100

    pff

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,66 @@
+<?php
+// Language: Français 
+// Module: infoEntry - 1.2.2
+// Date: 2018-02-19 14:15:58 
+// Translated with dcTranslater - 2018.02.14 
+
+#_public.php:65
+$GLOBALS['__l10n']['Next entry: %s'] = 'Billet suivant&nbsp;: %s';
+
+#_public.php:68
+$GLOBALS['__l10n']['Previous entry: %s'] = 'Billet précédent&nbsp;: %s';
+
+#_public.php:71
+$GLOBALS['__l10n']['Next entry in category: %s'] = 'Billet suivant dans la catégorie&nbsp;: %s';
+
+#_public.php:74
+$GLOBALS['__l10n']['Previous entry in category: %s'] = 'Billet précédent dans la catégorie&nbsp;: %s';
+
+#_widgets.php:25
+$GLOBALS['__l10n']['InfoEntry: information about entry'] = 'InfoEntry : informations sur le billet';
+
+#_widgets.php:27
+$GLOBALS['__l10n']['Display all information on current entry'] = 'Afficher toutes les informations sur le billet courant';
+
+#_widgets.php:28
+$GLOBALS['__l10n']['Information about entry'] = 'Informations sur le billet';
+
+#_widgets.php:29
+$GLOBALS['__l10n']['Display author'] = 'Afficher le lien vers l\'auteur';
+
+#_widgets.php:30
+$GLOBALS['__l10n']['Display date'] = 'Afficher la date';
+
+#_widgets.php:31
+$GLOBALS['__l10n']['Date format (leave empty to use default format):'] = 'Format de la date (laisser vide pour utiliser le format par défaut) :';
+
+#_widgets.php:32
+$GLOBALS['__l10n']['Display category'] = 'Afficher le lien vers la catégorie';
+
+#_widgets.php:33
+$GLOBALS['__l10n']['Display feed'] = 'Afficher le lien du fil';
+
+#_widgets.php:34
+$GLOBALS['__l10n']['Feed format:'] = 'Format du fil :';
+
+#_widgets.php:35
+$GLOBALS['__l10n']['RSS'] = 'RSS';
+
+#_widgets.php:35
+$GLOBALS['__l10n']['Atom'] = 'Atom';
+
+#_widgets.php:37
+$GLOBALS['__l10n']['Display tags'] = 'Afficher les mots-clés';
+
+#_widgets.php:44
+$GLOBALS['__l10n']['Display next entry'] = 'Afficher le lien vers le billet suivant';
+
+#_widgets.php:45
+$GLOBALS['__l10n']['Display previous entry'] = 'Afficher le lien vers le billet précédent';
+
+#_widgets.php:46
+$GLOBALS['__l10n']['Display next same category\'s entry'] = 'Afficher le lien vers le billet suivant de la catégorie';
+
+#_widgets.php:47
+$GLOBALS['__l10n']['Display previous same category\'s entry'] = 'Afficher le lien vers le billet précédent de la catégorie';
+
